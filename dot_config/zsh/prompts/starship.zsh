0b1f58c004c5979e230679969e93be2d876e569e
noeltz <noeltz+noreply@github.com>
2026-06-12 13:39:05 +0200

    rework zssh starship init

--- dot_config/zsh/prompts/starship.zsh
+++ dot_config/zsh/prompts/starship.zsh
@@ -1,7 +1,10 @@
 #!/usr/bin/env zsh
 
+# Only load Starship in graphical sessions
 if command -v starship &>/dev/null; then
-  eval "$(starship init zsh --print-full-init)"
-  export STARSHIP_CACHE=$XDG_CACHE_HOME/starship
-  export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/starship.toml
+    if [[ -n "$DISPLAY" ]] || [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
+    eval "$(starship init zsh --print-full-init)"
+    export STARSHIP_CACHE=$XDG_CACHE_HOME/starship
+    export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/starship.toml
+    fi
 fi
