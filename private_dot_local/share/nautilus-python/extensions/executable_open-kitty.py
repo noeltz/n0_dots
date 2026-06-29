6b0fc8cc0e3acd7960e16997ae67ddcc4c75ac70
noeltz <noeltz@web.de>
2026-06-29 13:38:59 +0200

    update

--- private_dot_local/share/nautilus-python/extensions/executable_open-kitty.py
+++ private_dot_local/share/nautilus-python/extensions/executable_open-kitty.py
@@ -1,29 +1,30 @@
 import os
 from urllib.parse import unquote
-
-from gi.repository import GObject, Nautilus
-
+from gi.repository import Nautilus, GObject
 
 class KittyExtension(GObject.GObject, Nautilus.MenuProvider):
-    def get_file_items(self, window, files):
+
+    def get_file_items(self, files):
+        # API 4.0: 'window' argument removed
         return []
 
-    def get_background_items(self, window, file):
-        if file.get_uri_scheme() != "file":
+    def get_background_items(self, folder):
+        # API 4.0: 'window' argument removed; 'file' is now 'folder'
+        if folder.get_uri_scheme() != 'file':
             return []
 
         item = Nautilus.MenuItem(
-            name="KittyExtension::OpenInKitty",
-            label="Open in Kitty",
-            tip="Open Kitty terminal in this directory",
+            name='KittyExtension::OpenInKitty',
+            label='Open in Kitty',
+            tip='Open Kitty terminal in this directory'
         )
-        item.connect("activate", self.on_item_activated, file)
+        item.connect('activate', self.on_item_activated, folder)
         return [item]
 
-    def on_item_activated(self, menu, file):
-        # Convert file URI to local path
-        filepath = unquote(file.get_uri()[7:])
-
-        # Spawn kitty in that directory
+    def on_item_activated(self, menu, folder):
+        # Convert folder URI to a local filesystem path
+        filepath = unquote(folder.get_uri()[7:])
+        
+        # Spawn kitty directly into the directory
         os.chdir(filepath)
-        os.system("kitty &")
+        os.system('kitty &')
